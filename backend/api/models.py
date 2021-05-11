3f67b2401f099cdab64c746cdcecd039e0fd9b24
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-05-11 21:06:15 +0430

    feat: is_active field & View

--- backend/api/models.py
+++ backend/api/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     slug = models.SlugField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.author} posted {self.title}'
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializers
 
 
-class PostViewSet(viewsets.ViewSet):
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.filter(is_active=True)
+    serializer_class = PostSerializers
     permission_classes = [IsAuthenticated, ]
-    serializer
