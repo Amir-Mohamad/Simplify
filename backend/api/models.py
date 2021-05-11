232165e7c02dbb3bdd0c5cbd18349b87f2f7389d
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-05-11 20:55:13 +0430

    feat: api configs & Post Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    """
+        The Main post model
+    """
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author} posted {self.title}'
---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
+router = DefaultRouter()
+router.register(r'', views.PostsViewSet, basename='posts_list')
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
